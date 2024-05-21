84614c8a2efd9eceae5dcad9fa1f140b7052e8b7
Jhumpter <joaopwii@gmail.com>
2024-05-21 18:48:17 -0300

    Update palindromo.py

--- Repetição/palindromo.py
+++ Repetição/palindromo.py
@@ -1,8 +1,13 @@
-frase = input('Digite uma frase: ')
+frase = input('Digite uma frase (sem acentos): ')
 frase = frase.replace(' ', '').lower()
-for c in range(0, int(len(frase)/2)):
-    ultimo = int(len(frase) - c)
-    if frase[0 - c] == frase[ultimo]:
-        print('Palíndromo')
+controle = 0
+for c in range(0, (len(frase))//2):
+    ultimo = len(frase) - 1 - c
+    if frase[0 + c] == frase[ultimo]:
+        controle += 0
     else:
-        print('Não é palíndromo')
+        controle += 1
+if controle == 0:
+    print('Palíndromo')
+else:
+    print('Não é palíndromo')
